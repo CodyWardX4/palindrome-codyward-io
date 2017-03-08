33a78af816bfb72faf0634edd642cde79191bced
IBM <cody.ward.x4@gmail.com>
2017-03-08 10:32:55 -0500

    Fixed bug that validated false positives

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,27 @@
 function displayText(){
+	// receive input from user and change to uppercase.
 	var userInput = document.getElementById("userInputField").value;
 	var userInput = userInput.toUpperCase();
 
+	// needed to display reversed input for user.
 	var displayInput = userInput;
-
-	var noSpacesNoCommas = userInput.replace(/[\s,.-[\]!?]+/g, '');
-	var splitInput = noSpacesNoCommas.split("");
-	var reverseInput = splitInput.reverse();
-	var rejoinInput = reverseInput.join("");
-
 	var displaySplit = displayInput.split("");
 	var displayReverse = displaySplit.reverse();
 	var displayRejoin = displayReverse.join("");
 
+	// valication continued...
+	var noExtras = userInput.replace(/[\?!-., ]+/g, '');
+	var splitInput = noExtras.split("");
+	var reverseInput = splitInput.reverse();
+	var rejoinInput = reverseInput.join("");
+
 	function checker(){
-		  if (noSpacesNoCommas == rejoinInput){
-		  		document.getElementById("userInput").innerHTML = "<span class='input-style'>" + userInput + "</span>" + " in reverse is " +
+		  if (noExtras == rejoinInput){
+		  		document.getElementById("userInput").innerHTML = "<span class='input-style'>" + displayInput + "</span>" + " in reverse is " +
 		  		"<span class='input-reverse'>" + displayRejoin + "</span>" + " and therefore, it " + "<span class='input-true'>" + "is" + "</span>" +
 		  		" a Palindrome.";
 		  } else {
-		      	document.getElementById("userInput").innerHTML = "<span class='input-style'>" + userInput + "</span>" + " in reverse is " +
+		      	document.getElementById("userInput").innerHTML = "<span class='input-style'>" + displayInput + "</span>" + " in reverse is " +
 		  		"<span class='input-reverse'>" + displayRejoin + "</span>" + " and therefore, it " + "<span class='input-false'>" + "is&nbsp;NOT" + "</span>" +
 		  		" a Palindrome.";
 		  }
